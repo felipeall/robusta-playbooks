9e354b7e044bbd81b36f539834b410a3221720c6
Felipe Allegretti <felipeallh@gmail.com>
2023-11-29 16:18:50 -0300

    Add debugging

--- my_playbook_repo/parse_job_failure.py
+++ my_playbook_repo/parse_job_failure.py
@@ -6,11 +6,26 @@ from robusta.api import action, JobChangeEvent, JsonBlock, RobustaJob
 
 @action
 def parse_job_failure(event: JobChangeEvent):
+    logging.info(f"JobChangeEvent triggered!")
+
     job: RobustaJob = event.get_job()
     job_name: str = job.metadata.name
+    logging.info(f"{job_name=}")
+    job_labels: dict = job.metadata.labels
+    logging.info(f"{job_labels=}")
 
-    logging.info(f"JobChangeEvent triggered! {job_name=} {job.metadata=}")
+    logging.info(f"{event=}")
+    logging.info(f"{event.get_context()=} {dir(event.get_context())}")
+    logging.info(f"{event.get_all_sinks()=} {dir(event.get_all_sinks())}")
+    logging.info(f"{event.get_resource()=} {dir(event.get_resource())}")
+    logging.info(f"{event.get_scheduler()=} {dir(event.get_scheduler())}")
+    logging.info(f"{event.get_source()=} {dir(event.get_source())}")
+    logging.info(f"{event.get_subject()=} {dir(event.get_subject())}")
+    logging.info(f"{event.description=}")
+    logging.info(f"{event.response=}")
+    logging.info(f"{event.operation=}")
+    logging.info(f"{event.all_sinks=}")
 
     event.add_enrichment([
-        JsonBlock(json.dumps({"job_name": job_name})),
+        JsonBlock(json.dumps({"job_name": job_name, "job_labels": job_labels})),
     ])

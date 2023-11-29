f4f9e67a0d35191f8e9411ed2fb8a2ca25f10959
Felipe Allegretti <felipeallh@gmail.com>
2023-11-29 17:09:08 -0300

    Refactoring

--- my_playbook_repo/parse_job_failure.py
+++ my_playbook_repo/parse_job_failure.py
@@ -1,31 +1,27 @@
 import json
 import logging
 
-from robusta.api import action, JobChangeEvent, JsonBlock, RobustaJob
+from pydantic import BaseModel
+from robusta.api import JobChangeEvent, JsonBlock, RobustaJob, action
+
+
+class JobInfo(BaseModel):
+    job_name: str
+    job_labels: dict
+
+    def dump(self) -> str:
+        return json.dumps(self.dict())
 
 
 @action
 def parse_job_failure(event: JobChangeEvent):
-    logging.info(f"JobChangeEvent triggered!")
+    logging.info("[parse_job_failure] JobChangeEvent triggered!")
 
     job: RobustaJob = event.get_job()
     job_name: str = job.metadata.name
-    logging.info(f"{job_name=}")
     job_labels: dict = job.metadata.labels
-    logging.info(f"{job_labels=}")
+    job_info: JobInfo = JobInfo(job_name=job_name, job_labels=job_labels)
 
-    logging.info(f"{event=}")
-    logging.info(f"{event.get_context()=} {dir(event.get_context())}")
-    logging.info(f"{event.get_all_sinks()=} {dir(event.get_all_sinks())}")
-    logging.info(f"{event.get_resource()=} {dir(event.get_resource())}")
-    logging.info(f"{event.get_scheduler()=} {dir(event.get_scheduler())}")
-    logging.info(f"{event.get_source()=} {dir(event.get_source())}")
-    logging.info(f"{event.get_subject()=} {dir(event.get_subject())}")
-    logging.info(f"{event.description=}")
-    logging.info(f"{event.response=}")
-    logging.info(f"{event.operation=}")
-    logging.info(f"{event.all_sinks=}")
+    logging.info(f"[parse_job_failure] Sending message to Kafka: {job_info.dict()}")
 
-    event.add_enrichment([
-        JsonBlock(json.dumps({"job_name": job_name, "job_labels": job_labels})),
-    ])
+    event.add_enrichment([JsonBlock(json_str=job_info.dump())])

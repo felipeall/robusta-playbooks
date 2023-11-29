4b22c94c2b0996ca3b13860c9823d034579625f6
Felipe Allegretti <felipeallh@gmail.com>
2023-11-29 18:19:50 -0300

    Parse job status

--- my_playbook_repo/parse_job_failure.py
+++ my_playbook_repo/parse_job_failure.py
@@ -1,27 +1,39 @@
 import json
-import logging
+import logging as log
+from typing import Tuple
 
 from pydantic import BaseModel
 from robusta.api import JobChangeEvent, JsonBlock, RobustaJob, action
 
 
 class JobInfo(BaseModel):
-    job_name: str
-    job_labels: dict
+    name: str
+    labels: dict
+    status: str
+    message: str
+    reason: str
 
     def dump(self) -> str:
         return json.dumps(self.dict())
 
 
+def __get_job_details(job: RobustaJob) -> Tuple[str, str, str]:
+    for condition in job.status.conditions:
+        if condition.status == "True":
+            return condition.type, condition.message, condition.reason
+    return "", "", ""
+
+
 @action
 def parse_job_failure(event: JobChangeEvent):
-    logging.info("[parse_job_failure] JobChangeEvent triggered!")
+    log.info("[parse_job_failure] JobChangeEvent triggered!")
 
     job: RobustaJob = event.get_job()
-    job_name: str = job.metadata.name
-    job_labels: dict = job.metadata.labels
-    job_info: JobInfo = JobInfo(job_name=job_name, job_labels=job_labels)
+    name: str = job.metadata.name
+    labels: dict = job.metadata.labels
+    status, message, reason = __get_job_details(job)
 
-    logging.info(f"[parse_job_failure] Sending message to Kafka: {job_info.dict()}")
+    job_info: JobInfo = JobInfo(name=name, labels=labels, status=status, message=message, reason=reason)
+    log.info(f"[parse_job_failure] Sending message to Kafka: {job_info.dict()}")
 
     event.add_enrichment([JsonBlock(json_str=job_info.dump())])
